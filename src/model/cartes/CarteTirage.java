5644860ac584853dce1fda2605d590b9237e912c
carraa <carraa@pc-dg-027-04>
2017-01-06 14:13:08 +0100

    la methode avancer et tout ce qui en découle

--- src/model/cartes/CarteTirage.java
+++ src/model/cartes/CarteTirage.java
@@ -1,6 +1,18 @@
 package model.cartes;
 
+import java.util.ArrayList;
 import model.ObjetIdentifie;
 
 public abstract class CarteTirage extends ObjetIdentifie {
+    
+    
+    /*
+    tas de carte de tresor monté des eau et action spé
+    */
+    
+//    private ArrayList<CarteTirage> tasDeCarte ;
+    
+    
+    
+    
 }
