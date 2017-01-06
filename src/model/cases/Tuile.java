b927e956136e7af5aa135cd3d872f194dc0a3d31
carraa <carraa@pc-dg-027-04>
2017-01-06 11:09:18 +0100

    la methode avancer et tout ce qui en découle

--- src/model/cases/Tuile.java
+++ src/model/cases/Tuile.java
@@ -3,6 +3,7 @@ package model.cases;
 import java.util.Collection;
 import model.ObjetIdentifie;
 import model.aventuriers.Aventurier;
+import util.Utils;
 import util.Utils.EtatTuile;
 
 /**
@@ -10,8 +11,22 @@ import util.Utils.EtatTuile;
  * @author IUT2-Dept Info
  */
 public class Tuile extends ObjetIdentifie {
+    private Utils.Tresor tresor ;
+    private String nomTuile ;
+    private EtatTuile etatCourant;
+    private Collection<Aventurier> aventuriers;
+
+    public Tuile(String nomTuile, EtatTuile etatCourant) {
+        this.tresor = tresor;
+        this.nomTuile = nomTuile;
+        this.etatCourant = etatCourant;
+        this.aventuriers = aventuriers;
+    }
     
-    EtatTuile etatCourant;
-    Collection<Aventurier> aventuriers;
+  
+    
+    
+    
+    
     
 }
