df76fba254eb387084ab96f30b770ad60b48e14b
carraa <carraa@pc-dg-027-04>
2017-01-06 14:30:01 +0100

    la methode avancer et tout ce qui en découle

--- src/model/cartes/CarteInondation.java
+++ src/model/cartes/CarteInondation.java
@@ -3,4 +3,42 @@ package model.cartes;
 import model.ObjetIdentifie;
 
 public class CarteInondation extends ObjetIdentifie {
+    
+    
+    private String nomCarteInondation ;
+
+    public CarteInondation(String nomCarteInondation) {
+        this.nomCarteInondation = nomCarteInondation;
+    }
+
+    CarteInondation c0 = new CarteInondation("Héliport") ;
+    CarteInondation c1 = new CarteInondation("La Caverne des Ombres") ;
+    CarteInondation c2 = new CarteInondation("La Forêt Pourpre");
+    CarteInondation c3 = new CarteInondation  ("La Porte de Bronze");
+    CarteInondation c4 = new CarteInondation  ("La Porte de Fer");
+    CarteInondation c5 = new CarteInondation  ("La Tour de Guet");
+    CarteInondation c6 = new CarteInondation  ("Le Jardin des MurMures");
+    CarteInondation c7 = new CarteInondation  ("Le Marais Brumeux");
+    CarteInondation c8 = new CarteInondation  ("Le Palais des Marées");
+    CarteInondation c9 = new CarteInondation  ("Le Rocher Fantôme");
+    CarteInondation c10 = new CarteInondation ("Les Falaises de L'Oubli");
+    CarteInondation c11 = new CarteInondation ("Le Temple du Soleil");
+    CarteInondation c12 = new CarteInondation ("L'Observatoire");
+    CarteInondation c13 =  new CarteInondation ("Le Val du Crépuscule");
+    CarteInondation c14 = new CarteInondation ("Le Temple de la Lune");
+    CarteInondation c15 = new CarteInondation ("Les Dunes de L'Illusion");
+    CarteInondation c16 = new CarteInondation ("Le Pont des Abîmes");
+    CarteInondation c17 = new CarteInondation ("Le Palais de Corail");
+    CarteInondation c18 = new CarteInondation  ("Le Lagon Perdu");
+    CarteInondation c19 = new CarteInondation  ("Le Jardin des Hurlements");
+    CarteInondation c20 = new CarteInondation  ("La Porte D'Or");
+    CarteInondation c21 = new CarteInondation  ("La Porte de Cuivre");
+    CarteInondation c22 = new CarteInondation  ("La Porte D'argent");
+    CarteInondation c23 = new CarteInondation  ("La Carverne du Brasier");
+    
+    
+    
+    
+    
+    
 }
