54bbd05eae772f329774a01a13e9d9ed5d1aa3d7
carraa <carraa@pc-dg-027-04>
2017-01-06 14:13:00 +0100

    la methode avancer et tout ce qui en découle

--- src/model/cartes/CarteTresor.java
+++ src/model/cartes/CarteTresor.java
@@ -4,4 +4,37 @@ package model.cartes;
 import model.cartes.CarteTirage;
 
 public class CarteTresor extends CarteTirage {
+    
+    private String nomCarte ;
+//    ArrayList<CarteTirage> carteTresors ;
+
+    public CarteTresor(String nomCarte) {
+        this.nomCarte = nomCarte;
+    }
+    
+    
+    CarteTresor pierre1 = new CarteTresor("PIERRE") ;
+    CarteTresor pierre2 = new CarteTresor("PIERRE") ;
+    CarteTresor pierre3 = new CarteTresor("PIERRE") ;
+    CarteTresor pierre4 = new CarteTresor("PIERRE") ;
+    CarteTresor pierre5 = new CarteTresor("PIERRE") ;    
+    
+    CarteTresor cristal1 = new CarteTresor("CRISTAL") ;
+    CarteTresor cristal2 = new CarteTresor("CRISTAL") ;
+    CarteTresor cristal3 = new CarteTresor("CRISTAL") ;
+    CarteTresor cristal4 = new CarteTresor("CRISTAL") ;
+    CarteTresor cristal5 = new CarteTresor("CRISTAL") ;
+
+    CarteTresor zephyr1 = new CarteTresor("ZEPHYR") ;
+    CarteTresor zephyr2 = new CarteTresor("ZEPHYR") ;
+    CarteTresor zephyr3 = new CarteTresor("ZEPHYR") ;
+    CarteTresor zephyr4 = new CarteTresor("ZEPHYR") ;
+    CarteTresor zephyr5 = new CarteTresor("ZEPHYR") ;
+    
+    CarteTresor calice1 = new CarteTresor("CALICE") ;
+    CarteTresor calice2 = new CarteTresor("CALICE") ;
+    CarteTresor calice3 = new CarteTresor("CALICE") ;
+    CarteTresor calice4 = new CarteTresor("CALICE") ;
+    CarteTresor calice5 = new CarteTresor("CALICE") ;    
+    
 }
